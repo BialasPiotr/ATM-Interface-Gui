2ab6da69681e86af2c1f8144ce3fe11258134534
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-05-02 10:54:40 +0200

    Window
    changed the layout of the window to include a text box displaying the current state of the user's account and a list of operations

--- ATM  inteface/src/ATMInterface.java
+++ ATM  inteface/src/ATMInterface.java
@@ -1,21 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
 import javax.swing.*;
 
 public class ATMInterface extends JFrame implements ActionListener {
     private JTextField inputField;
     private JTextArea outputArea;
     private JButton depositButton;
+    private JButton withdrawButton;
     private JButton balanceButton;
-    private JButton exitButton;
     private JButton clearHistoryButton;
+    private JButton exitButton;
+    private JLabel accountBalanceLabel;
     private double balance = 0;
-    private ArrayList<Transaction> transactionList = new ArrayList<>();
+    private DefaultListModel<String> transactionListModel = new DefaultListModel<String>();
 
     public ATMInterface() {
         super("ATM Interface");
-        LoginWindow loginWindow = new LoginWindow(this);
+
+        // create and set up components
+        JLabel amountLabel = new JLabel("Enter amount:");
         inputField = new JTextField(10);
         inputField.addActionListener(this);
 
@@ -24,17 +27,27 @@ public class ATMInterface extends JFrame implements ActionListener {
 
         depositButton = new JButton("Deposit");
         depositButton.addActionListener(this);
-        JButton withdrawButton = new JButton("Withdraw");
+
+        withdrawButton = new JButton("Withdraw");
         withdrawButton.addActionListener(this);
+
         balanceButton = new JButton("Balance");
         balanceButton.addActionListener(this);
-        exitButton = new JButton("Exit");
-        exitButton.addActionListener(this);
+
         clearHistoryButton = new JButton("Clear History");
         clearHistoryButton.addActionListener(this);
 
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(this);
+
+        accountBalanceLabel = new JLabel(String.format("Account Balance: $%.2f", balance));
+
+        JList<String> transactionList = new JList<String>(transactionListModel);
+        JScrollPane transactionListScrollPane = new JScrollPane(transactionList);
+
+        // set up layout
         JPanel inputPanel = new JPanel(new FlowLayout());
-        inputPanel.add(new JLabel("Enter amount:"));
+        inputPanel.add(amountLabel);
         inputPanel.add(inputField);
 
         JPanel buttonPanel = new JPanel(new GridLayout(5, 1, 10, 10));
@@ -44,11 +57,16 @@ public class ATMInterface extends JFrame implements ActionListener {
         buttonPanel.add(clearHistoryButton);
         buttonPanel.add(exitButton);
 
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel.add(accountBalanceLabel, BorderLayout.NORTH);
+        rightPanel.add(transactionListScrollPane, BorderLayout.CENTER);
+
         JPanel contentPane = new JPanel(new BorderLayout());
         contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         contentPane.add(inputPanel, BorderLayout.NORTH);
         contentPane.add(new JScrollPane(outputArea), BorderLayout.CENTER);
-        contentPane.add(buttonPanel, BorderLayout.EAST);
+        contentPane.add(buttonPanel, BorderLayout.WEST);
+        contentPane.add(rightPanel, BorderLayout.EAST);
 
         setContentPane(contentPane);
         pack();
@@ -61,24 +79,26 @@ public class ATMInterface extends JFrame implements ActionListener {
             double amount = Double.parseDouble(inputField.getText());
             balance += amount;
             Transaction transaction = new Transaction("Deposit", amount, balance);
-            transactionList.add(transaction);
+            transactionListModel.addElement(transaction.toString());
             outputArea.append(transaction.toString() + "\n");
+            accountBalanceLabel.setText(String.format("Account Balance: $%.2f", balance));
             inputField.setText("");
-        } else if (e.getActionCommand().equals("Withdraw")) {
+        } else if (e.getSource() == withdrawButton) {
             double amount = Double.parseDouble(inputField.getText());
             if (amount > balance) {
                 JOptionPane.showMessageDialog(this, "Insufficient funds.");
             } else {
                 balance -= amount;
                 Transaction transaction = new Transaction("Withdrawal", amount, balance);
-                transactionList.add(transaction);
+                transactionListModel.addElement(transaction.toString());
                 outputArea.append(transaction.toString() + "\n");
+                accountBalanceLabel.setText(String.format("Account Balance: $%.2f", balance));
             }
             inputField.setText("");
         } else if (e.getSource() == balanceButton) {
             JOptionPane.showMessageDialog(this, String.format("Your current balance is: $%.2f", balance));
         } else if (e.getSource() == clearHistoryButton) {
-            transactionList.clear();
+            transactionListModel.clear();
             outputArea.setText("");
         } else if (e.getSource() == exitButton) {
             System.exit(0);
